9420400b0700678652703cec134e21db1d9a70c7
EzequielMoreira <eezequielmoreira@gmail.com>
2025-02-18 18:52:19 -0300

    ejemplos outerText insertados

--- modificar-contenido-del-DOM/texto-textContent-vs-InnerText/codigo.js
+++ modificar-contenido-del-DOM/texto-textContent-vs-InnerText/codigo.js
@@ -3,9 +3,9 @@
 // Suponiendo que tenemos un HTML con un etiqueta div con su clase container o contenedor.
 // Lo que hacemos en el codigo js para utilizar esta propiedad es lo siguiente.
 
-const element = document.querySelector("div");
+// const element = document.querySelector("div");
 
-element.nodeName;  // Aqui como resultado nos arroja en la consola la etiqueta DIV.
+// element.nodeName;  // Aqui como resultado nos arroja en la consola la etiqueta DIV.
 
 
 
@@ -28,3 +28,13 @@ elemen.innerText;
 // Hi everyone
 // Mi name is
 // More information
+
+
+
+//                  *** outerText ***
+
+const element = document.querySelector(".container");
+
+element.innerText = "Mi name es";
+
+element.outerText = "Me llamo ???"
