a9df0d80c23ea6c7fe899b00f6fd879ddc9d90b6
EzequielMoreira <eezequielmoreira@gmail.com>
2025-02-18 15:05:07 -0300

    creacion de codigo js

---
+++ modificar-contenido-del-DOM/texto-textContent-vs-InnerText/codigo.js
@@ -0,0 +1,8 @@
+//                  *** .nodeName ***
+
+// Suponiendo que tenemos un HTML con un etiqueta div con su clase container o contenedor.
+// Lo que hacemos en el codigo js para utilizar esta propiedad es lo siguiente.
+
+const element = document.querySelector("div");
+
+element.nodeName;  // Aqui como resultado nos arroja en la consola la etiqueta DIV.
