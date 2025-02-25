ab309fdebcf47830391456dcaf9c7e8ef10e9ecf
EzequielMoreira <eezequielmoreira@gmail.com>
2025-02-24 23:45:11 -0300

    element.id colocado

--- modificar-contenido-del-DOM/atributos-html-de-elementos/codigo.js
+++ modificar-contenido-del-DOM/atributos-html-de-elementos/codigo.js
@@ -1,1 +1,3 @@
 const element = document.querySelector("div"); //Aqui capturamos el elemento div con la clase container
+
+element.id = "page" //Capturamos el id del elemento y su clase, id = page, class = container
