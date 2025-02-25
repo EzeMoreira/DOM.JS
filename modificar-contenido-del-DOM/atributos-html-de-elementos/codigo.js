69f1ec021bd8a781f5ec7bb93045bf6104d375d3
EzequielMoreira <eezequielmoreira@gmail.com>
2025-02-24 23:46:30 -0300

    style y classname agregado en js

--- modificar-contenido-del-DOM/atributos-html-de-elementos/codigo.js
+++ modificar-contenido-del-DOM/atributos-html-de-elementos/codigo.js
@@ -1,3 +1,5 @@
 const element = document.querySelector("div"); //Aqui capturamos el elemento div con la clase container
 
 element.id = "page" //Capturamos el id del elemento y su clase, id = page, class = container
+element.style = "color: red" //Colocacion de estilo
+element.className = "data"; //Ademas del estilo, aqui renombramos la clase con el nombre data
