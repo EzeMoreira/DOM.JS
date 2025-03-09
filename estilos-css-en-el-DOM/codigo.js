6e21e84699c0afd47e6d1a98122c0dd39a38e13b
EzequielMoreira <eezequielmoreira@gmail.com>
2025-03-09 16:41:40 -0300

    modificacion en codigo js con dark y light mode

--- estilos-css-en-el-DOM/codigo.js
+++ estilos-css-en-el-DOM/codigo.js
@@ -7,3 +7,14 @@ div.getAttribute("class") // "element shine dark-mode"
 // Para modificar clases CSS
 div.className = "element shine dark-mode";
 div.setAttribute("class", "element shine dark-mode");
+
+
+// Como cambiar de dark-mode a light-mode
+
+const div = document.querySelector(".element");
+
+// Obtener clases CSS
+div.className;  // "element shine dark-mode"
+const classnames = div.className.split(" "); // ["element", "shine", "dark-mode"]
+classnames[2] = "light-mode";
+div.className = classnames.join(" "); // element shine light-mode
